3fc70c9856e91888d358179841ba647090dcdad4
Robert Abraham <robert@adeven.com>
2013-09-26 14:00:56 +0000

    added testing examples

---
+++ test/gocheck_test.go
@@ -0,0 +1,18 @@
+package hello_test
+
+import (
+	. "github.com/matttproud/gocheck"
+	"testing"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestHelloWorld(c *C) {
+	c.Check(42, Equals, "42")
+	c.Check(42, Equals, 42)
+}
